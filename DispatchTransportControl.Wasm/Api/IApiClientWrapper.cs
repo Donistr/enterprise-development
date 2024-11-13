4e35a75b2ce110669352e13c464a3974dd8caefc
Donistr <deniselag@mail.ru>
2024-11-13 21:35:59 +0400

    feat: сгенерировал для клиентской части ApiClient по документации и написал для него обёртку

---
+++ DispatchTransportControl.Wasm/Api/IApiClientWrapper.cs
@@ -0,0 +1,15 @@
+﻿using DispatchTransportControl.Shared.DTO;
+
+namespace DispatchTransportControl.Wasm.Api;
+
+public interface IApiClientWrapper
+{
+    Task<ICollection<DriverDto>> GetDrivers();
+    Task<DriverDto> GetDriver(int id);
+    Task<DriverDto> CreateDriver(DriverCreateDto driverDto);
+    Task<DriverDto> UpdateDriver(DriverDto driverDto);
+    Task DeleteDriver(int id);
+    Task<ICollection<DriverDto>> GetDriversByPeriod(TimePeriodDto timePeriodDto);
+    Task<ICollection<DriverTripCountDto>> GetTop5DriversByTripCount();
+    Task<ICollection<DriverTripStatsDto>> GetDriverTripStats(TimePeriodDto timePeriodDto);
+}
