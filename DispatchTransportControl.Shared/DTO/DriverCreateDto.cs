cc95014bb336c584a76be7dec4b93a3e158f91e8
Donistr <deniselag@mail.ru>
2024-11-15 14:07:20 +0400

    feat: добавил валидацию, чтобы нельзя было создавать записи с пустыми полями

--- DispatchTransportControl.Shared/DTO/DriverCreateDto.cs
+++ DispatchTransportControl.Shared/DTO/DriverCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace DispatchTransportControl.Shared.DTO;
 
@@ -11,41 +12,48 @@ public class DriverCreateDto
     ///     Имя
     /// </summary>
     [JsonPropertyName("name")]
+    [Required(ErrorMessage = "Имя обязательно для заполнения")]
     public required string Name { get; set; }
 
     /// <summary>
     ///     Фамилия
     /// </summary>
     [JsonPropertyName("surname")]
+    [Required(ErrorMessage = "Фамилия обязательна для заполнения")]
     public required string Surname { get; set; }
 
     /// <summary>
     ///     Отчество
     /// </summary>
     [JsonPropertyName("patronymic")]
+    [Required(ErrorMessage = "Отчество обязательно для заполнения")]
     public required string Patronymic { get; set; }
 
     /// <summary>
     ///     Паспорт
     /// </summary>
     [JsonPropertyName("passport")]
+    [Required(ErrorMessage = "Паспорт обязателен для заполнения")]
     public required string Passport { get; set; }
 
     /// <summary>
     ///     Водительское удостоверение
     /// </summary>
     [JsonPropertyName("driver_license")]
+    [Required(ErrorMessage = "Водительское удостоверение обязательно для заполнения")]
     public required string DriverLicense { get; set; }
 
     /// <summary>
     ///     Адрес
     /// </summary>
     [JsonPropertyName("address")]
+    [Required(ErrorMessage = "Адрес обязателен для заполнения")]
     public required string Address { get; set; }
 
     /// <summary>
     ///     Телефон
     /// </summary>
     [JsonPropertyName("phone")]
+    [Required(ErrorMessage = "Телефон обязателен для заполнения")]
     public required string Phone { get; set; }
 }
